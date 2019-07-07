6d86af68f895019b1f7ce8d77a32fe83f8ddd6f3
simahao <simahao@sohu.com>
2019-07-07 08:21:25 -0700

    jdb&jstat

---
+++ src/main/java/com/dce/dev2/Jdb.java
@@ -0,0 +1,23 @@
+package main.java.com.dce.dev2;
+
+/**
+ * Jdb
+ */
+public class Jdb {
+    public static int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+  public static void main(String[] args) {
+      int i = 5;
+      int j = 6;
+      int sum = add(i, j);
+      System.out.println(sum);
+          
+      sum = 0;
+      for(i = 0; i < 100; i++) {
+          sum += i;
+      }
+      System.out.println(sum);        
+    }
+}
---
+++ src/main/java/com/dce/dev2/Jstat.java
@@ -0,0 +1,21 @@
+package main.java.com.dce.dev2;
+
+/**
+ * Jstat
+ */
+public class Jstat {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            byte[] b1 = new byte[1024 * 1024];
+            byte[] b2 = new byte[1024 * 1024];
+            byte[] b3 = new byte[1024 * 1024];
+            byte[] b4 = new byte[1024 * 1024];
+            byte[] b5 = new byte[1024 * 1024];
+            byte[] b6 = new byte[1024 * 1024];
+            b3 = null;
+            b5 = null;
+            b6 = null;
+        } 
+    }
+}
