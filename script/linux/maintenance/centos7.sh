a385266b9a56a5a5fd8a4b3975d6596868dd2606
simahao <simahao@sohu.com>
2020-07-11 14:00:51 +0800

    [feat] add install_other_tools

--- script/linux/maintenance/centos7.sh
+++ script/linux/maintenance/centos7.sh
@@ -5,12 +5,23 @@
 # 2.compatible with CentOS7.x
 # @author:zhanghao
 # @groupId:dev2
-# @version 1.0
-# @date 20190731
 #########################################################
+
+# ------------------------------------------------------
+# version |  date    | comments                         |
+# ------------------------------------------------------
+# 1.0     | 20190731 | init                             |
+# 1.1     | 20200710 | 1.add install_other_tools        |
+#         |          | 2.rename function name           |
+#-------------------------------------------------------|
+
 #Source function library.
 . /etc/init.d/functions
 . ~/.bash_profile
+# functions don't include below path in which some components would install
+PATH=$PATH:/usr/local/bin:/usr/local/sbin
+export TERM=xterm
+
 #date
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
 #ip
@@ -23,7 +34,7 @@ USER=$(whoami)
 DISK_SDA=$(df -h | grep -w "/" | grep -v 'host' | awk '{print $5}')
 #cpu_average_check,1min 5min 15min
 CPU_AVG=$(cat /proc/loadavg | awk '{print $1, $2, $3}')
-#cpu processor 
+#cpu processor
 CPU_PROCESSORS=$(cat /proc/cpuinfo | grep -w 'processor' | wc -l)
 #cpu cores
 #in case of `` awk should like this:awk -F ':\\\\s*' '{print $2}'
@@ -40,6 +51,15 @@ KERNEL_VERSION=$(uname -sr)
 JAVA_VERSION=$(java -version 2>&1 | awk 'NR==1{gsub(/"/,""); print $3}')
 #gcc_version
 GCC_VERSION=$(gcc --version | grep -w "gcc" | awk '{print $3}')
+#dev2 ftp server
+SSH_IP="192.168.128.128"
+# SSH_IP="172.52.145.172"
+#ftp user
+SSH_USER="share"
+#ftp pass 
+SSH_PASS="Dev2_ftp"
+#ssh path
+SSH_PATH="/home/share/ftp/zhanghao"
 
 #set LANG
 export LANG=zh_CN.UTF-8
@@ -49,17 +69,17 @@ export LANG=zh_CN.UTF-8
 uid=`id | cut -d\( -f1 | cut -d= -f2`
 if [[ $uid != 0 ]]; then
   action "Please run this script as root." /bin/false
-  exit 1
+  # exit 1
 fi
 
 
 #add user and give sudoers
-addUser() {
+add_user() {
   echo "========================add user========================="
   #add user
   while true
-  do  
-    read -p "user name:" name 
+  do
+    read -p "user name:" name
     NAME=`awk -F':' '{print $1}' /etc/passwd | grep -wx $name 2>/dev/null | wc -l`
     if [[ $NAME == "" ]]; then
       echo "user name is null, please input angain"
@@ -103,11 +123,11 @@ addUser() {
       cp /etc/sudoers /etc/sudoers.$(date +%F)
       SUDO=`grep -w "$name" /etc/sudoers | wc -l`
       if [[ $SUDO == 0 ]]; then
-          echo "$name  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-          echo "run cmd:tail -1 /etc/sudoers"
-          tail -1 /etc/sudoers
-          # grep -w "$name" /etc/sudoers
-          sleep 1
+        echo "$name  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+        echo "run cmd:tail -1 /etc/sudoers"
+        tail -1 /etc/sudoers
+        # grep -w "$name" /etc/sudoers
+        sleep 1
       fi
       action "add sudoer successfully"  /bin/true
     fi
@@ -119,7 +139,7 @@ addUser() {
 }
 
 #config Yum CentOS7-aliyun.repo
-configYum() {
+config_yum() {
   echo "==============config yum with aliyun repo================"
   for i in /etc/yum.repos.d/*.repo
   do
@@ -140,20 +160,20 @@ configYum() {
 }
 
 #Charset zh_CN.UTF-8
-configCharset() {
+config_charset() {
   echo "==============config LC_CTYPE=zh_CN.UTF-8================"
 
   #config root
   linenum=$(grep -wn "^LC_CTYPE" ~/.bash_profile | awk -F ':' '{print $1}')
   if [[ $linenum != 0 && $linenum != "" ]]; then
-    sed -in "${linenum}c LC_CTYPE=zh_CN.UTF-8" ~/.bash_profile 
+    sed -in "${linenum}c LC_CTYPE=zh_CN.UTF-8" ~/.bash_profile
   else
-    echo "LC_CTYPE=zh_CN.UTF-8" >> ~/.bash_profile 
+    echo "LC_CTYPE=zh_CN.UTF-8" >> ~/.bash_profile
   fi
   . ~/.bash_profile
-  action "config root with LC_CTYPE=zh_CN.UTF-8 successfully" /bin/true 
+  action "config root with LC_CTYPE=zh_CN.UTF-8 successfully" /bin/true
   #config other users
-  read -p "config other users with LC_CTYPE?[y/n]:" option 
+  read -p "config other users with LC_CTYPE?[y/n]:" option
   if [[ $option == "y" || $option == "Y" ]]; then
     for user in `ls /home`
     do
@@ -166,22 +186,22 @@ configCharset() {
         echo "config for user:${user}"
         linenum=$(grep -wn "^LC_CTYPE" /home/$user/.bash_profile | awk -F ':' '{print $1}')
         if [[ $linenum != 0 && $linenum != "" ]]; then
-          sed -in "${linenum}c LC_CTYPE=zh_CN.UTF-8" /home/$user/.bash_profile 
+          sed -in "${linenum}c LC_CTYPE=zh_CN.UTF-8" /home/$user/.bash_profile
         else
-          echo "LC_CTYPE=zh_CN.UTF-8" >> /home/$user/.bash_profile 
+          echo "LC_CTYPE=zh_CN.UTF-8" >> /home/$user/.bash_profile
         fi
         . /home/$user/.bash_profile
       fi
     done
   fi
-  action "config other users with LC_CTYPE=zh_CN.UTF-8 successfully" /bin/true 
+  action "config other users with LC_CTYPE=zh_CN.UTF-8 successfully" /bin/true
   echo "========================================================="
   echo ""
   sleep 2
 }
 
 #Close Selinux and Iptables
-configFirewall() {
+config_firewall() {
   echo "==========forbidden selinux and close iptables==========="
   cp /etc/selinux/config /etc/selinux/config.$(date +%F)
   systemctl stop firewalld
@@ -190,8 +210,8 @@ configFirewall() {
   setenforce 0
   systemctl status firewalld
   iptables -F
-  echo 'run cmd:grep SELINUX=disabled /etc/selinux/config ' 
-  grep SELINUX=disabled /etc/selinux/config 
+  echo 'run cmd:grep SELINUX=disabled /etc/selinux/config '
+  grep SELINUX=disabled /etc/selinux/config
   echo 'run cmd:getenforce '
   getenforce
   echo 'run cmd:iptables -L'
@@ -203,7 +223,7 @@ configFirewall() {
 }
 
 #Change sshd default port to 22
-configDefaultSSHPort() {
+config_default_ssh_port() {
   echo "======confirm ssh port is 22,if not,change to 22========="
   port=$(grep -wE '^Port' /etc/ssh/sshd_config | awk -F '\\s+' '{print $2}')
   if [[ $port != 22 && $port != "" ]]; then
@@ -212,14 +232,14 @@ configDefaultSSHPort() {
     systemctl restart sshd.service && action "config ssh port successfully" /bin/true || action "config ssh port failed" /bin/false
   else
     echo "change nothing,ssh port has been set to 22"
-  fi 
+  fi
   echo "========================================================="
   echo ""
   sleep 2
 }
 
 #setting history
-configHistory() {
+config_history() {
   echo "=============history command size=2000==================="
   linenum=$(grep -wn "^HISTSIZE" /etc/profile | awk -F ':' '{print $1}')
   if [[ $linenum != 0 && $linenum != "" ]]; then
@@ -240,11 +260,11 @@ configHistory() {
 }
 
 #install tools
-installTools() {
-  echo "======install sysstat|dos2unix|openssl|openssh|bash======"
+install_sys_tools() {
+  echo "======install sysstat|dos2unix|openssl|openssh|bash|ftp======"
   ping -c 2 mirrors.aliyun.com
   sleep 1
-  yum install sysstat dos2unix openssl openssh bash -y
+  yum install sysstat dos2unix openssl openssh bash ftp -y
   sleep 1
   action "install tools successfully" /bin/true
   echo "========================================================="
@@ -253,7 +273,7 @@ installTools() {
 }
 
 #Adjust the file descriptor(limits.conf)
-configLimits() {
+config_limits() {
   echo "===============config file descriptor===================="
   LIMIT=`grep nofile /etc/security/limits.conf | grep -v "^#" | wc -l`
   if [[ $LIMIT == 0 ]]; then
@@ -265,7 +285,7 @@ configLimits() {
     echo ""
     cp /etc/security/limits.conf /etc/security/limits.conf.$(date +%F)
     cat >> /etc/security/limits.conf<<EOF
-* soft nofile 65535 
+* soft nofile 65535
 * hard nofile 65535
 EOF
   fi
@@ -297,7 +317,7 @@ EOF
 
 
 #Optimizing the system kernel
-configSysctl() {
+config_sysctl() {
   echo "====================config sysctl========================"
   SYSCTL=`grep "net.ipv4.tcp" /etc/sysctl.conf | wc -l`
   if [[ $SYSCTL < 1 ]]; then
@@ -314,13 +334,13 @@ net.ipv4.tcp_synack_retries = 1
 net.ipv4.tcp_max_orphans = 16384
 net.ipv4.tcp_rmem=4096 87380 4194304
 net.ipv4.tcp_wmem=4096 16384 4194304
-net.ipv4.tcp_keepalive_time = 600 
-net.ipv4.tcp_keepalive_probes = 5 
-net.ipv4.tcp_keepalive_intvl = 15 
+net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp_keepalive_probes = 5
+net.ipv4.tcp_keepalive_intvl = 15
 net.ipv4.route.gc_timeout = 100
-net.ipv4.ip_local_port_range = 1024 65000 
+net.ipv4.ip_local_port_range = 1024 65000
 net.ipv4.icmp_echo_ignore_broadcasts=1
-net.core.somaxconn = 16384 
+net.core.somaxconn = 16384
 net.core.netdev_max_backlog = 16384"
     echo "***************************************"
     read -p "is this ok?[y/n]:" option
@@ -337,13 +357,13 @@ net.ipv4.tcp_synack_retries = 1
 net.ipv4.tcp_max_orphans = 16384
 net.ipv4.tcp_rmem=4096 87380 4194304
 net.ipv4.tcp_wmem=4096 16384 4194304
-net.ipv4.tcp_keepalive_time = 600 
-net.ipv4.tcp_keepalive_probes = 5 
-net.ipv4.tcp_keepalive_intvl = 15 
+net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp_keepalive_probes = 5
+net.ipv4.tcp_keepalive_intvl = 15
 net.ipv4.route.gc_timeout = 100
-net.ipv4.ip_local_port_range = 1024 65000 
+net.ipv4.ip_local_port_range = 1024 65000
 net.ipv4.icmp_echo_ignore_broadcasts=1
-net.core.somaxconn = 16384 
+net.core.somaxconn = 16384
 net.core.netdev_max_backlog = 16384
 EOF
       sysctl -p && action "kernel setting successfully" /bin/true || action "kernel setting failed" /bin/false
@@ -355,7 +375,7 @@ EOF
 }
 
 #time sync
-configOuterSyncTime() {
+config_outer_sync_time() {
   echo "================config internet time sync================"
   NTPDATE=`grep ntpdate /var/spool/cron/root 2>/dev/null | wc -l`
   if [[ $NTPDATE == 0 ]]; then
@@ -367,14 +387,14 @@ configOuterSyncTime() {
     echo "#times sync by script at $(date +%F)" >> /var/spool/cron/root
     echo "*/5 * * * * /usr/sbin/ntpdate time.windows.com &>/dev/null" >> /var/spool/cron/root
   fi
-  echo 'run cmd:crontab -l'  
+  echo 'run cmd:crontab -l'
   crontab -l
   action "config time sync successfully" /bin/true
   echo "========================================================="
   echo ""
   sleep 2
 }
-configInnerSyncTime() {
+config_inner_sync_time() {
   echo "================config intranet time sync================"
   sync="ntpd"
   while true
@@ -389,7 +409,7 @@ configInnerSyncTime() {
         ;;
       "ntpc")
         sync=
-        readIp "" "ntp client ip list"
+        read_ip "" "ntp client ip list"
         ;;
       *)
         break
@@ -413,7 +433,7 @@ configInnerSyncTime() {
     cat > /etc/ntp.conf <<EOF
 driftfile /var/lib/ntp/drift
 restrict default kod nomodify notrap nopeer noquery
-restrict 127.0.0.1 
+restrict 127.0.0.1
 restrict ::1
 server 127.127.1.0
 fudge  127.127.1.0 stratum 10
@@ -458,12 +478,12 @@ EOF
     "
   done
   [ $? == 0 ] && action "config intranet time sync completely" /bin/true || action "config intranet time sync failed" /bin/false
-  echo 
+  echo
   echo "========================================================="
   echo ""
   sleep 2
 }
-readName() {
+read_name() {
   read -p "input authorized user name that you want to:" name
   if [[ $name == "" ]]; then
     read -p "input user name is null, quit?[y/n]:" option
@@ -476,7 +496,7 @@ readName() {
   fi
   step=$1
 }
-readIp() {
+read_ip() {
   read -p "input $2, for example ip1,ip2,ip3:" ips
   if [[ $ips == "" ]]; then
     read -p "input ip address lists is null, quit?[y/n]:" option
@@ -484,7 +504,7 @@ readIp() {
       step="quit"
       return 1
     else
-      readIp $1 $2
+      read_ip $1 $2
     fi
   fi
   iplist=(${ips//,/ })
@@ -514,7 +534,7 @@ readIp() {
   step=$1
 }
 
-configAuthorization() {
+config_authorization() {
   echo "==================Authorization=========================="
   echo ""
   echo "*************************************************************************************************"
@@ -530,7 +550,7 @@ configAuthorization() {
   do
     case $step in
       "ip")
-        readIp "auth" "ip list"
+        read_ip "auth" "ip list"
         ;;
       "auth")
         break
@@ -539,10 +559,10 @@ configAuthorization() {
         break
         ;;
       "name")
-        readName "ip"
+        read_name "ip"
         ;;
       *)
-        readName "ip"
+        read_name "ip"
         ;;
     esac
   done
@@ -568,7 +588,7 @@ configAuthorization() {
         ssh-copy-id -o StrictHostKeyChecking=no ${name}@${iplist[j]};
         "
         echo ""
-        sleep 1 
+        sleep 1
       done
     done
   fi
@@ -579,7 +599,7 @@ configAuthorization() {
 
 
 # set backspace as erase for root and all login users(/home/*)
-configBackspace() {
+config_backspace() {
   echo "================config backspace as erase================"
   echo "config for user:root"
   erase=`grep -wx "stty erase ^H" /root/.bash_profile | wc -l`
@@ -612,6 +632,164 @@ configBackspace() {
   sleep 2
 }
 
+ftp_check() {
+  which ftp > /dev/null 2>&1
+  if [[ $? != 0 ]]; then
+    yum install ftp
+  fi
+}
+
+install_sshpass() {
+  which sshpass > /dev/null 2>&1
+  if [[ $? > 0 ]]; then
+    echo -e "\033[36m install sshpass firstly\033[0m"
+    ftp_check
+    ftp -inv <<EOF
+open ${SSH_IP}
+user ${SSH_USER} ${SSH_PASS} 
+bin
+cd zhanghao
+mget sshpass-1.05.tar.gz 
+quit
+EOF
+    tar zxf sshpass-1.05.tar.gz && cd sshpass-1.05
+    ./configure && make && sudo make install
+    sshpass -V > /dev/null 2>&1 && action "install sshpass successfully" /bin/true || action "install sshpass successfully" /bin/false
+    cd .. && rm -rf sshpass-1.05
+  fi
+}
+
+scp_get() {
+  if [[ $1 == "ftp" ]]; then
+    local FILE=$2
+    sshpass -p ${SSH_PASS} scp -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_IP}:${SSH_PATH}/${FILE} .
+  elif [[ $1 == "cmd" ]]; then
+    local CMD=$2
+    sshpass -p ${SSH_PASS} ssh -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_IP} "${CMD}"
+  else
+    echo "unsupport parameter:$1 for scp_get"
+  fi
+}
+
+install_arthas() {
+  install_sshpass
+  scp_get ftp 'arthas*.zip'
+  if [[ $? == 0 ]]; then
+    unzip -d tmp arthas*.zip
+    rm -rf arthas*.zip > /dev/null 2>&1
+    chmod 755 tmp/*.sh && chmod 755 tmp/*.jar && sudo cp -r tmp/* /usr/bin && rm -rf tmp
+    [ $? == 0 ] && action "install arthas successfully" /bin/true || action "install arthas failed" /bin/false
+  fi
+}
+
+install_nvim() {
+  scp_get "nvim-0.4.3.appimage"
+  chmod u+x nvim-0.4.3.appimage && ./nvim-0.4.3.appimage --appimage-extract && sudo cp -r squashfs-root/usr/* /usr
+  [ $? == 0 ] && action "install nvim successfully" /bin/true || action "install nvim failed" /bin/false
+  rm -rf squashfs-root
+}
+
+install_vscodeserver() {
+  VERSION_INFO="$(scp_get cmd "cd ${SSH_PATH};ls vscode-server*.gz")"
+  VERSION_INFO=(${VERSION_INFO})
+  VERSION_ARR=($(echo "${VERSION_INFO[@]}" | tr " +" '\n' | sort -n))
+  while true
+  do
+    echo ""
+    local COUNT=${#VERSION_ARR[@]}
+    local NUM=1
+    local VERSION=
+    for i in "${VERSION_ARR[@]}"; do
+      VERSION=${i//vscode-server-linux-x64-/}
+      VERSION=${VERSION//.tar.gz/}
+      echo -e "\033[36m(${NUM}) install ${VERSION}"
+      ((NUM=NUM+1))
+    done
+    echo -e "\033[36m(0) exit\033[0m"
+    read -p "Please enter your choice[0-${COUNT}]: " input1
+    case "$input1" in
+      0)
+        clear
+        return 
+        ;;
+      1|2|3|4|5)
+        break
+        ;;
+      *)
+        echo "----------------------------------"
+        echo "|   Please enter right choice!   |"
+        echo "----------------------------------"
+        sleep 1
+        clear
+        ;;
+    esac
+  done
+  ((IDX=$input1-1))
+  scp_get ftp "${VERSION_ARR[${IDX}]}"
+  ls vscode*.gz > /dev/null 2>&1 || action "install vscode-server failed" /bin/false || return
+
+  VERSION="${VERSION_ARR[${IDX}]}"
+  VERSION=${VERSION//vscode-server-linux-x64-/}
+  VERSION=${VERSION//.tar.gz/}
+  COMMITID=$(scp_get cmd "cd ${SSH_PATH};grep ${VERSION} commitid")
+  COMMITID=$(echo ${COMMITID} | awk '{print $2}')
+  [ ! -z ${COMMITID} ] || action "install vscode-server failed" /bin/false || return
+
+  read -p "Please enter the account list under which you want to install,e.x. dce,root,mt: " input1
+  local USER_LIST=(${input1//,/ })
+  local ERR_FALG=0
+  local USER=
+  for USER in "${USER_LIST[@]}"; do
+    id ${USER} > /dev/null 2>&1
+    if [[ $? > 0 ]]; then
+      echo "user:${USER} is not exist, install vscode-server under ${USER} failed"
+      ((ERR_FALG=${ERR_FALG}+1))
+      continue
+    fi
+    rm -rf /home/${USER}/.vscode-server/bin/${COMMITID}
+    mkdir -p /home/${USER}/.vscode-server/bin/${COMMITID} && \
+    tar zxf "${VERSION_ARR[${IDX}]}" -C /home/${USER}/.vscode-server/bin/${COMMITID} --strip-components=1 && \
+    chown -R ${USER}:${USER} /home/${USER}/.vscode-server
+    [ $? -gt 0 ] && ((ERR_FALG=${ERR_FALG}+1)) || echo "install vscode-server under ${USER} successfully"
+  done
+  [ ${ERR_FALG} -eq 0 ] && action "install vscode-server task successfully" /bin/true || action "install vscode-server task failed" /bin/false
+  rm -rf "${VERSION_ARR[${IDX}]}"
+}
+
+install_other_tools() {
+  while true
+  do
+    echo ""
+    echo -e "\033[36m(1) install sshpass"
+    echo -e "\033[36m(2) install arthas"
+    echo -e "\033[36m(3) install vscode-server"
+    echo -e "\033[36m(0) exit\033[0m"
+    read -p "Please enter your choice[0-3]: " input1
+    case "$input1" in
+      0)
+        clear
+        break
+        ;;
+      1)
+        install_sshpass
+        ;;
+      2)
+        install_arthas
+        ;;
+      3)
+        install_vscodeserver
+        ;;
+      *)
+        echo "----------------------------------"
+        echo "|   Please enter right choice!   |"
+        echo "----------------------------------"
+        sleep 1
+        clear
+        ;;
+    esac
+  done
+}
+
 clear
 echo ""
 echo -e "\033[36m|--------------------System Infomation----------------------"
@@ -636,7 +814,8 @@ do
   echo -e "\033[36m*==========================================================*"
   echo -e "\033[36m*                    Dev2 Linux Utility                    *"
   echo -e "\033[36m*                                                          *"
-  echo -e "\033[36m* Color Support:secureCRT->Terminal->Emulation->ANSI Color *"
+  echo -e "\033[36m* Color Support:                                           *"
+  echo -e "\033[36m* secureCRT->Terminal->Emulation->(Xterm+ANSI Color)       *"
   echo -e "\033[36m*==========================================================*"
   echo -e "\033[36m(1)  新建用户并选择是否加入sudoers"
   echo -e "\033[36m(2)  外网配置YUM源(aliyun) *内网不要运行!!!*"
@@ -651,62 +830,62 @@ do
   echo -e "\033[36m(11) 设置内网集群时间同步"
   echo -e "\033[36m(12) 集群环境双向授信 "
   echo -e "\033[36m(13) 设置backspace为删除键"
+  echo -e "\033[36m(14) 安装其他工具"
   echo -e "\033[36m(0)  退出 \033[0m"
-  read -p "Please enter your choice[0-13]: " input1
+  read -p "Please enter your choice[0-14]: " input1
   case "$input1" in
     0)
       clear
-      break 
+      break
       ;;
     1)
-      addUser
+      add_user
       ;;
     2)
-      configYum
+      config_yum
       ;;
     3)
-      configCharset
+      config_charset
       ;;
     4)
-      configFirewall
+      config_firewall
       ;;
     5)
-      configDefaultSSHPort
+      config_default_ssh_port
       ;;
     6)
-      configHistory
+      config_history
       ;;
     7)
-      installTools 
+      install_sys_tools
       ;;
     8)
-      configLimits
+      config_limits
       ;;
     9)
-      configSysctl
+      config_sysctl
       ;;
     10)
-      configOuterSyncTime
+      config_outer_sync_time
       ;;
     11)
-      configInnerSyncTime
+      config_inner_sync_time
       ;;
     12)
-      configAuthorization
+      config_authorization
       ;;
     13)
-      configBackspace
+      config_backspace
+      ;;
+    14)
+      install_other_tools
       ;;
     *)
       echo "----------------------------------"
       echo "|   Please enter right choice!   |"
       echo "----------------------------------"
       sleep 1
-      # for i in `seq -w 2 -1 1`
-      # do 
-      #   echo -ne "\b\b$i"
-      #   sleep 1
-      # done
       clear
+      ;;
   esac
 done
